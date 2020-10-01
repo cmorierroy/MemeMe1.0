5782a77a660db26f096776a42091f3e94c0b3609
Cédric Morier-Roy <cmorierroy@gmail.com>
2020-10-01 13:00:38 -0500

    Added permissions to access camera and photo library, added outlets, disabled camera button if device has no camera

--- MemeMe1.0/MemeEditorView.swift
+++ MemeMe1.0/MemeEditorView.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class MemeEditorView: UIViewController {
 
+    //nav bar button outlets
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    
+    //Image view outlet
+    @IBOutlet weak var imageView: UIImageView!
+    
+    //Text field outlets
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
+    
+    //Toolbar button outlets
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var libraryButton: UIBarButtonItem!
+    
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -17,6 +33,9 @@ class MemeEditorView: UIViewController {
     override func viewWillAppear(_ animated: Bool)
     {
         super.viewWillAppear(animated)
+        
+        //Disable camera button if there is no camera on device.
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
                 
     }
     
